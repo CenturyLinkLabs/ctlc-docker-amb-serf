ed0ab8ee5ef0c16dc4373dc7ba12dc556246586b
Lucas Carlson <lucas@rufy.com>
2014-04-07 14:07:38 -0700

    Fixing the registry

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN rm serf.zip
 RUN mv serf /usr/bin/
 
 ADD /serf-join.sh /serf-join.sh
+ADD /start-socat.sh /start-socat.sh
 ADD /start-serf-service-registry.sh /start-serf-service-registry.sh
 ADD /supervisord-serf-service-registry.conf /etc/supervisor/conf.d/supervisord-serf-service-registry.conf
 RUN chmod 755 /*.sh
--- start-serf-service-registry.sh
+++ start-serf-service-registry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/export LOCAL_PORT=\1\nexport REMOTE_HOST=\2\nexport REMOTE_PORT=\3/' > /tmp/env
+env | grep _TCP= | grep -v SERF | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/export LOCAL_PORT=\1\nexport REMOTE_HOST=\2\nexport REMOTE_PORT=\3/' > /tmp/env
 
 echo "export LOCAL_HOST=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`" >> /tmp/env
 
