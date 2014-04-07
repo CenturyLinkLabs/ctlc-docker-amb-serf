c8349a3927baa407e2c53dc384576668eb9f6bec
Lucas Carlson <lucas@rufy.com>
2014-04-07 11:26:06 -0700

    Tagging the serf agent with information

--- start-serf-service-registry.sh
+++ start-serf-service-registry.sh
@@ -1,1 +1,8 @@
-exec serf agent -role serf-agent
+#!/bin/bash
+env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/export LOCAL_PORT=\1\nexport REMOTE_HOST=\2\nexport REMOTE_PORT=\3/' > /tmp/env
+
+echo "export LOCAL_HOST=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`" >> /tmp/env
+
+source /tmp/env
+
+exec serf agent -role serf-agent -tag role=${NAME:-$LOCAL_PORT}
